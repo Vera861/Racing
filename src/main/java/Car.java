f3c3c40c7cafbd6dae8741bccf99d301dadda92d
Vera <mverynchik@gmail.com>
2021-05-22 15:55:13 +0300

    Lesson 5 Core 3 homeWork

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,13 +1,14 @@
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+
 public class Car implements Runnable {
 
     private static int CARS_COUNT;
-    static {
-        CARS_COUNT = 0;
-    }
-
     private Race race;
     private int speed;
     private String name;
+    private final CyclicBarrier beginRace;
+    private final CountDownLatch endRace;
 
     public String getName() {
         return name;
@@ -17,9 +18,11 @@ public class Car implements Runnable {
         return speed;
     }
 
-    public Car(Race race, int speed) {
+    public Car(Race race, int speed, CyclicBarrier beginRace,CountDownLatch endRace) {
         this.race = race;
         this.speed = speed;
+        this.beginRace=beginRace;
+        this.endRace=endRace;
         CARS_COUNT++;
         this.name = "Участник #" + CARS_COUNT;
     }
@@ -28,14 +31,16 @@ public class Car implements Runnable {
     public void run() {
         try {
             System.out.println(this.name + " готовится");
-            Thread.sleep(500 + (int)(Math.random() * 800));
+            Thread.sleep(500 + (int) (Math.random() * 800));
             System.out.println(this.name + " готов");
+            beginRace.await();
         } catch (Exception e) {
             e.printStackTrace();
         }
         for (int i = 0; i < race.getStages().size(); i++) {
             race.getStages().get(i).go(this);
         }
+            endRace.countDown();
     }
 }
 
--- src/main/java/Race.java
+++ src/main/java/Race.java
@@ -9,6 +9,7 @@ public class Race {
     }
 
     public Race(Stage... stages) {
+
         this.stages = new ArrayList<>(Arrays.asList(stages));
     }
 }
